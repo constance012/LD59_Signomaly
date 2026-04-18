9858edde4334ec0545b52a06dc4163baaea5ef3f
Lam Van Quoc <lvq13302@gmail.com>
2026-04-18 20:18:54 +0700

    feat(timer-logic): implement logic to start, stop, and count down timers, setup main level timer in scene.

---
+++ Signomaly/Assets/Scripts/System/Timers/MainLevelTimer.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+
+namespace AforgeStudios.Signomaly
+{
+	public class MainLevelTimer : TimerBase
+	{
+		public static MainLevelTimer Instance { get; private set; }
+
+		protected override void Awake()
+		{
+			base.Awake();
+			MakeSingleton();
+		}
+
+		private void Update()
+		{
+#if UNITY_EDITOR
+			DebugTimer();
+#endif
+		}
+
+		public void SubstractTime(TimeSpan timeToSubtract)
+		{
+			_duration -= timeToSubtract;
+		}
+
+		protected override void OnTimerComplete()
+		{
+			Debug.Log("Level ends!");
+		}
+
+		private void MakeSingleton()
+		{
+			if (Instance == null)
+			{
+				Instance = this;
+			}
+			else
+			{
+				Destroy(this.gameObject);
+			}
+		}
+
+#if UNITY_EDITOR
+		private void DebugTimer()
+		{
+			if (Input.GetKeyDown(KeyCode.PageUp))
+			{
+				StartTimer();
+			}
+			if (Input.GetKeyDown(KeyCode.Delete))
+			{
+				SubstractTime(TimeSpan.FromSeconds(_debugSubtractSeconds));
+			}
+		}
+#endif
+	}
+}
