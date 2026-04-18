b19be770af83101d5e1842e40efafbb1a296ba75
Lam Van Quoc <lvq13302@gmail.com>
2026-04-18 19:16:22 +0700

    chore(scene): setup a simple scene layout with the player and puzzle objects.

---
+++ Signomaly/Assets/Scripts/Player/SimplePlayerController.cs
@@ -0,0 +1,77 @@
+using System;
+using CSTGames.SharedResources;
+using UnityEngine;
+
+namespace AforgeStudios.Signomaly
+{
+	[RequireComponent(typeof(Rigidbody))]
+	public class SimplePlayerController : MonoBehaviour, IPlayerController
+	{
+		[Header("References"), Space]
+		[SerializeField] private Rigidbody rb;
+		[SerializeField] private Stats stats;
+
+		[Header("Movement Settings"), Space]
+		[SerializeField] private float acceleration;
+		[SerializeField] private float deceleration;
+
+		public static Vector3 Position { get; private set; }
+		public static event Action<Vector3> OnVelocityChanged;
+
+		private Vector3 _movementDirection;
+		private Vector3 _previousDirection;
+		private float _maxSpeed;
+		private float _currentSpeedSquared;
+
+		private void Start()
+		{
+			_maxSpeed = stats.GetDynamicStat(StatType.MoveSpeed);
+		}
+
+		private void Update()
+		{
+			ReadInputValues();
+
+			if (_movementDirection.sqrMagnitude > .01f)
+				_previousDirection = _movementDirection;
+		}
+
+		private void FixedUpdate()
+		{
+			UpdateVelocity();
+
+			Position = rb.position;
+		}
+
+		public void ReadInputValues()
+		{
+			_movementDirection.x = LegacyInputManager.Instance.GetAxisRaw("Horizontal");
+			_movementDirection.z = LegacyInputManager.Instance.GetAxisRaw("Vertical");
+			_movementDirection.Normalize();
+		}
+
+		public void UpdateVelocity()
+		{
+			if (_movementDirection.sqrMagnitude > .01f)
+			{
+				Vector3 targetSpeed = _movementDirection * _maxSpeed;
+				rb.linearVelocity = Vector3.MoveTowards(rb.linearVelocity, targetSpeed, acceleration * Time.deltaTime);
+			}
+
+			else if (_currentSpeedSquared > 0f)
+			{
+				Vector3 restVelocity = new Vector3
+				{
+					x = 0f,
+					y = rb.linearVelocity.y,
+					z = 0f
+				};
+				
+				rb.linearVelocity = Vector3.MoveTowards(rb.linearVelocity, restVelocity, deceleration * Time.deltaTime);
+			}
+
+			OnVelocityChanged?.Invoke(rb.linearVelocity);
+			_currentSpeedSquared = rb.linearVelocity.sqrMagnitude;
+		}
+	}
+}
