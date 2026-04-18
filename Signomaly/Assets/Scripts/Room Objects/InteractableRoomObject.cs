843e813d18fcbfb45177923e80f732e785e2e357
Lam Van Quoc <lvq13302@gmail.com>
2026-04-18 22:30:40 +0700

    feat(object-interaction): implement core logic to check for interaction and add prefab for distraction objects.

---
+++ Signomaly/Assets/Scripts/Room Objects/InteractableRoomObject.cs
@@ -0,0 +1,60 @@
+using CSTGames.SharedResources;
+using UnityEngine;
+
+namespace AforgeStudios.Signomaly
+{
+	public abstract class InteractableRoomObject : MonoBehaviour
+	{
+		[Header("Shared Settings"), Space]
+		[SerializeField] protected string _objectName;
+		[SerializeField] protected float _interactRadius = 2f;
+		[SerializeField] protected int _maximumOverlappedColliders = 1;
+		[SerializeField] protected LayerMask _playerLayer;
+
+		public string ObjectName => _objectName;
+
+		protected Collider[] _overlappedColliders;
+
+		protected virtual void Awake()
+		{
+			SetupComponents();
+		}
+
+		protected virtual void LateUpdate()
+		{
+			CheckForPlayerInteraction();
+		}
+
+		protected virtual void SetupComponents()
+		{
+			_maximumOverlappedColliders = Mathf.Max(1, _maximumOverlappedColliders);
+			_overlappedColliders = new Collider[_maximumOverlappedColliders];
+		}
+
+		protected void CheckForPlayerInteraction()
+		{
+			int count = Physics.OverlapSphereNonAlloc(transform.position, _interactRadius, _overlappedColliders, _playerLayer);
+			
+			if (count > 0)
+			{
+				ReadPlayerInput();
+			}
+		}
+
+		protected void ReadPlayerInput()
+		{
+			if (LegacyInputManager.Instance.GetKeyDown(KeybindingAction.Interact))
+			{
+				Interact();
+			}
+		}
+
+		protected virtual void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawWireSphere(transform.position, _interactRadius);
+		}
+
+		public abstract void Interact();
+	}
+}
