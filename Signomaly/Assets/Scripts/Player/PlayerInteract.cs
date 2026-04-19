1aef92d54f78885caa672a8ffc91c216295a0802
DD <>
2026-04-19 19:41:44 +0700

    fix: add namespace AforgeStudios.Signomaly

--- Signomaly/Assets/Scripts/Player/PlayerInteract.cs
+++ Signomaly/Assets/Scripts/Player/PlayerInteract.cs
@@ -4,115 +4,118 @@ using CSTGames.SharedResources;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerInteract : MonoBehaviour
+namespace AforgeStudios.Signomaly
 {
-    [Header("References"), Space]
-    [SerializeField] Camera playerCamera;
-    [SerializeField] Transform objectGrabTransform;
-
-    [Header("Keybinds"), Space]
-    [SerializeField] private Key interactKey = Key.E;
-    [SerializeField] private Key dropKey = Key.Q;
-
-    [Header("Attributes"), Space]
-    [SerializeField] private float interactRange = 2f;
-
-    private Transform grabbingObjTransform;
-
-    private void Update()
+    public class PlayerInteract : MonoBehaviour
     {
-        //Pick Up Object
-        if (NewInputManager.Instance.GetKeyDown(interactKey))
+        [Header("References"), Space]
+        [SerializeField] Camera playerCamera;
+        [SerializeField] Transform objectGrabTransform;
+
+        [Header("Keybinds"), Space]
+        [SerializeField] private Key interactKey = Key.E;
+        [SerializeField] private Key dropKey = Key.Q;
+
+        [Header("Attributes"), Space]
+        [SerializeField] private float interactRange = 2f;
+
+        private Transform grabbingObjTransform;
+
+        private void Update()
         {
-            IInteractable interactable = GetClosetInteractableObject(GetInteractableList());
-            if (interactable != null && !interactable.GetLockInteract() && CheckObjectBoundsInCamera(interactable.GetTransform().GetComponent<MeshRenderer>()) && grabbingObjTransform == null)
+            //Pick Up Object
+            if (NewInputManager.Instance.GetKeyDown(interactKey))
             {
-                interactable.Interact(transform);
-                grabbingObjTransform = interactable.GetTransform();
-                PickUpObj(grabbingObjTransform);
+                IInteractable interactable = GetClosetInteractableObject(GetInteractableList());
+                if (interactable != null && !interactable.GetLockInteract() && CheckObjectBoundsInCamera(interactable.GetTransform().GetComponent<MeshRenderer>()) && grabbingObjTransform == null)
+                {
+                    interactable.Interact(transform);
+                    grabbingObjTransform = interactable.GetTransform();
+                    PickUpObj(grabbingObjTransform);
+                }
+            }
+
+            if (NewInputManager.Instance.GetKeyDown(dropKey))
+            {
+                if(grabbingObjTransform != null)
+                {
+                    DropDownObj(grabbingObjTransform);
+                }
             }
         }
 
-        if (NewInputManager.Instance.GetKeyDown(dropKey))
+        private void PickUpObj(Transform pickupObject)
         {
-            if(grabbingObjTransform != null)
+            if(pickupObject.TryGetComponent<DragableObject>(out DragableObject dragable))
             {
-                DropDownObj(grabbingObjTransform);
-            }
-        }
-    }
-
-    private void PickUpObj(Transform pickupObject)
-    {
-        if(pickupObject.TryGetComponent<DragableObject>(out DragableObject dragable))
-        {
-            dragable.PickObj(objectGrabTransform);
-        }
-    }
-
-    private void DropDownObj(Transform pickupObject)
-    {
-        if(pickupObject.TryGetComponent<DragableObject>(out DragableObject dragable))
-        {
-            dragable.DropObj();
-        }
-    }
-
-    public List<IInteractable> GetInteractableList()
-    {
-        List<IInteractable> interactableList = new List<IInteractable>();
-
-        Collider[] colliders = Physics.OverlapSphere(transform.position, interactRange);
-        foreach (Collider collider in colliders)
-        {
-            if (collider.TryGetComponent(out IInteractable interactableObj))
-            {
-                interactableList.Add(interactableObj);
+                dragable.PickObj(objectGrabTransform);
             }
         }
 
-        return interactableList;
-    }
-
-    public IInteractable GetClosetInteractableObject(List<IInteractable> interactables)
-    {
-        IInteractable closetInteractableObj = null;
-
-        foreach (IInteractable obj in interactables)
+        private void DropDownObj(Transform pickupObject)
         {
-            if (closetInteractableObj == null)
-                closetInteractableObj = obj;
-            else
+            if(pickupObject.TryGetComponent<DragableObject>(out DragableObject dragable))
             {
+                dragable.DropObj();
+            }
+        }
 
-                if (Vector3.Distance(transform.position, closetInteractableObj.GetTransform().position)
-                > Vector3.Distance(transform.position, obj.GetTransform().position))
+        public List<IInteractable> GetInteractableList()
+        {
+            List<IInteractable> interactableList = new List<IInteractable>();
+
+            Collider[] colliders = Physics.OverlapSphere(transform.position, interactRange);
+            foreach (Collider collider in colliders)
+            {
+                if (collider.TryGetComponent(out IInteractable interactableObj))
+                {
+                    interactableList.Add(interactableObj);
+                }
+            }
+
+            return interactableList;
+        }
+
+        public IInteractable GetClosetInteractableObject(List<IInteractable> interactables)
+        {
+            IInteractable closetInteractableObj = null;
+
+            foreach (IInteractable obj in interactables)
+            {
+                if (closetInteractableObj == null)
                     closetInteractableObj = obj;
+                else
+                {
+
+                    if (Vector3.Distance(transform.position, closetInteractableObj.GetTransform().position)
+                    > Vector3.Distance(transform.position, obj.GetTransform().position))
+                        closetInteractableObj = obj;
+                }
             }
+
+            return closetInteractableObj;
         }
 
-        return closetInteractableObj;
-    }
+        public bool CheckObjectBoundsInCamera(MeshRenderer reder)
+        {
+            Plane[] planes = GeometryUtility.CalculateFrustumPlanes(playerCamera);
 
-    public bool CheckObjectBoundsInCamera(MeshRenderer reder)
-    {
-        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(playerCamera);
+            if(GeometryUtility.TestPlanesAABB(planes, reder.bounds))
+                return true;
+            
+            return false;
+        }
 
-        if(GeometryUtility.TestPlanesAABB(planes, reder.bounds))
-            return true;
-        
-        return false;
-    }
+        public Key GetInteractKey()
+        {
+            return interactKey;
+        }
 
-    public Key GetInteractKey()
-    {
-        return interactKey;
-    }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-
-        Gizmos.DrawWireSphere(transform.position, interactRange);
+            Gizmos.DrawWireSphere(transform.position, interactRange);
+        }
     }
 }
