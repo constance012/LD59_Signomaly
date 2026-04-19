1790fb414524279499de6c7708ddbc35857d9e7d
Lam Van Quoc <lvq13302@gmail.com>
2026-04-19 23:23:39 +0700

    feat(room-objects): integrate pickup and drop logic allowing the player to pick them up and move around.

--- Signomaly/Assets/Scripts/Pickable Objects/PickableObjectPhysicHandler.cs
+++ Signomaly/Assets/Scripts/Pickable Objects/PickableObjectPhysicHandler.cs
@@ -6,7 +6,6 @@ namespace AforgeStudios.Signomaly
     {
         [Header("References"), Space]
         [SerializeField] private Rigidbody rb;
-        [SerializeField] private Collider col;
         [SerializeField] PickableObjectBehaviour iO;
 
         private Transform target;
@@ -16,7 +15,6 @@ namespace AforgeStudios.Signomaly
             this.target = transform;
             rb.useGravity = false;
             rb.isKinematic = true;
-            col.enabled = false;
         }
 
         public void DropObj()
@@ -24,7 +22,6 @@ namespace AforgeStudios.Signomaly
             this.target = null;
             rb.useGravity = true;
             rb.isKinematic = false;
-            col.enabled = true;
             iO.SetCanShowUI(true);
         }
 
