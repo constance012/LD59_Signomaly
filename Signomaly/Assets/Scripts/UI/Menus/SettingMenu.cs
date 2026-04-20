5526522f2087848091443c9d8bfa9ba95da95eee
DD <>
2026-04-20 22:27:00 +0700

    feat: add menu prefab and add resolution setting.

--- Signomaly/Assets/Scripts/UI/Menus/SettingMenu.cs
+++ Signomaly/Assets/Scripts/UI/Menus/SettingMenu.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 namespace AforgeStudios.Signomaly
 {
@@ -11,6 +14,12 @@ namespace AforgeStudios.Signomaly
         [SerializeField] private GameObject audioContainerGameObj;
         [SerializeField] private TextMeshProUGUI videoTitle;
         [SerializeField] private TextMeshProUGUI audioTitle;
+        [SerializeField] private TMP_Dropdown resolutionDropdown;
+
+        [Header("Audio Mixer"), Space]
+        [SerializeField] private AudioMixer mixer;
+
+        private Resolution[] resolutions;
 
         private void Start()
         {
@@ -25,6 +34,7 @@ namespace AforgeStudios.Signomaly
 
         private void GameManager_SettingMenu_OnSettingGame()
         {
+            ShowResolutionDropDown();
             containerGameObj.SetActive(true);
         }
 
@@ -62,6 +72,52 @@ namespace AforgeStudios.Signomaly
         public void BackButtonClick()
         {
             GameManager.Instance.PauseGame();
+        }
+
+        public void SetMasterVolume(float amount)
+        {
+            
+        }
+        public void SetMusicVolume(float amount)
+        {
+            
+        }
+        public void SetSoundVolume(float amount)
+        {
+            
+        }
+        public void SetAmbienceVolume(float amount)
+        {
+            
+        }
+
+        public void SetResolution(int resolutionIndex)
+        {
+            Resolution resolution = resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        }
+        
+        private void ShowResolutionDropDown()
+        {
+            resolutions = Screen.resolutions;
+
+            resolutionDropdown.ClearOptions();
+
+            List<string> options = new List<string>();
+            
+            int currentResolutionIndex = 0;
+            for(int i = 0; i < resolutions.Length; i++)
+            {
+                string option = resolutions[i].width + " x " + resolutions[i].height;
+                options.Add(option);
+
+                if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                    currentResolutionIndex = i;
+            }
+
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
         }
     }   
 }
