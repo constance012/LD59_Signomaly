738a794a7bbea167873587e00a9131600f34962d
Lam Van Quoc <lvq13302@gmail.com>
2026-04-20 17:33:12 +0700

    fix(player-movement): fix the slippery issue with the player movement after releasing input key.

--- Signomaly/Assets/Scripts/Player/ThreeDPlayerController.cs
+++ Signomaly/Assets/Scripts/Player/ThreeDPlayerController.cs
@@ -9,27 +9,24 @@ namespace AforgeStudios.Signomaly
         [Header("References"), Space]
         [SerializeField] private Rigidbody rb;
         [SerializeField] private Transform orientation;
+        [SerializeField] private Stats stats;
 
-        [Header("Attributes"), Space]
-        [SerializeField] private float moveSpeed;
-        [SerializeField] private float groundDrag;
-
-        [Header("Ground Check"), Space]
-        [SerializeField] private float playerHeight;
-        [SerializeField] private LayerMask groundLayer;
+        [Header("Movement Settings"), Space]
+        [SerializeField] private float acceleration;
+        [SerializeField] private float deceleration;
 
         private Vector3 _inputValues;
-        private bool _isGrounded;
+        private float _maxSpeed;
+        private float _currentSpeedSquared;
+
+        private void Start()
+        {
+            _maxSpeed = stats.GetDynamicStat(StatType.MoveSpeed);
+        }
 
         private void Update()
         {
             ReadInputValues();
-
-            CheckForGround();
-
-            SpeedControl();
-
-            ApplyDrag();
         }
 
         private void FixedUpdate()
@@ -38,47 +35,37 @@ namespace AforgeStudios.Signomaly
         }
 
         public void ReadInputValues()
-		{
-			var inputVector = NewInputManager.Instance.ReadValue<Vector2>(KeybindingAction.Movement);
+        {
+            var inputVector = NewInputManager.Instance.ReadValue<Vector2>(KeybindingAction.Movement);
 
-			_inputValues.x = inputVector.x;
-			_inputValues.y = inputVector.y;
-			_inputValues.Normalize();
-		}
+            _inputValues.x = inputVector.x;
+            _inputValues.y = inputVector.y;
+            _inputValues.Normalize();
+        }
 
         public void UpdateVelocity()
         {
             Vector3 moveDirection = orientation.forward * _inputValues.y + orientation.right * _inputValues.x;
 
-            if(_isGrounded)
-                rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-        }
-
-        private void SpeedControl()
-        {
-            Vector3 flatVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
-
-            //Limit velocity if needed
-            if (flatVelocity.magnitude > moveSpeed)
+            if (_inputValues.sqrMagnitude > .01f)
             {
-                Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
-                rb.linearVelocity = new Vector3(limitedVelocity.x, rb.linearVelocity.y, limitedVelocity.z);
-            }
-        }
-
-        private void CheckForGround()
-        {
-            _isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * .5f + .2f, groundLayer);
-        }
-
-        private void ApplyDrag()
-        {
-            if(_isGrounded)
-            {
-                rb.linearDamping = groundDrag;
+                Vector3 targetSpeed = moveDirection * _maxSpeed;
+                rb.linearVelocity = Vector3.MoveTowards(rb.linearVelocity, targetSpeed, acceleration * Time.deltaTime);
             }
 
-            rb.linearDamping = 0;
+            else if (_currentSpeedSquared > 0f)
+            {
+                Vector3 restVelocity = new Vector3
+                {
+                    x = 0f,
+                    y = rb.linearVelocity.y,
+                    z = 0f
+                };
+                
+                rb.linearVelocity = Vector3.MoveTowards(rb.linearVelocity, restVelocity, deceleration * Time.deltaTime);
+            }
+
+            _currentSpeedSquared = rb.linearVelocity.sqrMagnitude;
         }
     }
 }
