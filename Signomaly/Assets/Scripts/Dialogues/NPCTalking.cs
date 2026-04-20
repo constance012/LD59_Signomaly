3ebf29bb3ba8c77e01c2579e93d80960889cf628
DD <>
2026-04-20 14:42:40 +0700

    chore: rename Npc Class and add prefab dialogue box

--- Signomaly/Assets/Scripts/Dialogues/NPCTalking.cs
+++ Signomaly/Assets/Scripts/Dialogues/NPCTalking.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 
 namespace AforgeStudios.Signomaly
 {
-    public class NPC : MonoBehaviour
+    public class NPCTalking : MonoBehaviour
     {
         // [Header("References"), Space]
         [SerializeField] private TextMeshProUGUI nameTextMesh;
