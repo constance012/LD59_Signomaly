cf4a0071b7be2d8dc1b7f4a50d63810fe84ef613
DD <>
2026-04-21 00:31:49 +0700

    feat: add start menu.

--- Signomaly/Assets/Scripts/UI/Menus/PauseMenu.cs
+++ Signomaly/Assets/Scripts/UI/Menus/PauseMenu.cs
@@ -9,7 +9,6 @@ namespace AforgeStudios.Signomaly
     {
         [Header("References"), Space]
         [SerializeField] private GameObject containerGameObj;
-        [SerializeField] private TextMeshProUGUI textMeshProUGUI;
 
         private void Start()
         {
